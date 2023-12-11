2783ac46b33e865790d133e7e06c2e4f1bec0bc6
Yura1231 <146677552+Yura1231@users.noreply.github.com>
2023-12-11 02:32:22 +0200

    Update Лабораторна 8.cpp

--- Лабораторна 8/Лабораторна 8.cpp
+++ Лабораторна 8/Лабораторна 8.cpp
@@ -2,26 +2,37 @@
 #include <vector>
 #include <algorithm>
 #include <numeric>
-using namespace std;
+
+
+bool isLessThan3(int x) {
+    return x < 3;
+}
+
+
+bool isNegative(int x) {
+    return x < 0;
+}
+
+
+bool isWithin20Percent(int x, int maxElement) {
+    return x != maxElement && std::abs(x - maxElement) <= 0.2 * maxElement;
+}
+
 int main() {
-    
     int n;
     std::cout << "Enter the size of the array: ";
     std::cin >> n;
 
-    
     std::vector<int> arr(n);
     std::cout << "Enter the elements of the array:" << std::endl;
     for (int i = 0; i < n; ++i) {
         std::cin >> arr[i];
     }
 
-    
-    int countLessThan3 = std::count_if(arr.begin(), arr.end(), [](int x) { return x < 3; });
+    int countLessThan3 = std::count_if(arr.begin(), arr.end(), isLessThan3);
     std::cout << "1. Number of elements less than 3: " << countLessThan3 << std::endl;
 
-   
-    auto lastNegative = std::find_if(arr.rbegin(), arr.rend(), [](int x) { return x < 0; });
+    auto lastNegative = std::find_if(arr.rbegin(), arr.rend(), isNegative);
     if (lastNegative != arr.rend()) {
         int sumAfterLastNegative = std::accumulate(lastNegative.base(), arr.end(), 0);
         std::cout << "2. Sum of integers after the last negative element: " << sumAfterLastNegative << std::endl;
@@ -30,28 +41,29 @@ int main() {
         std::cout << "2. No negative elements found." << std::endl;
     }
 
-    
     auto maxElement = *std::max_element(arr.begin(), arr.end());
     std::transform(arr.begin(), arr.end(), arr.begin(), [maxElement](int x) {
         if (x != maxElement && std::abs(x - maxElement) <= 0.2 * maxElement) {
             return x;
         }
         else {
-            return maxElement + 1; 
+            return maxElement + 1;
         }
         });
 
-   
     auto partitionPoint = std::partition(arr.begin(), arr.end(), [maxElement](int x) {
-        return x != maxElement && std::abs(x - maxElement) <= 0.2 * maxElement;
+        return isWithin20Percent(x, maxElement);
         });
 
-    
     std::cout << "3. Transformed array:" << std::endl;
     for (const auto& element : arr) {
         std::cout << element << " ";
     }
     std::cout << std::endl;
+
+    return 0;
+}
+
 
     return 0;
 }
